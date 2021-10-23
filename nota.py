1b01f6c403e94b9f9cfb85df753ac154e37da5c0
CarlaPerr <perroudcarla@gmail.com>
2021-10-22 21:35:12 -0300

    Commit Inicial

---
+++ nota.py
@@ -0,0 +1,17 @@
+# #! /usr/bin/python3
+
+
+#Guardamos en una variable la proxima id disponible
+
+
+
+#creamos una clase Nota, que represente una nota en el anotador. Debe tener Texto y etiquetas y se puede buscar por texto. 
+
+
+#Crear el metodo iniciador. que inicie con la nota con un texto y opcionalmete, con etiquetas separadas por espacios. 
+
+
+#Crear un metodo que determine si la nota coincide con el filtro de busqueda. Retorna True o False. 
+
+
+
